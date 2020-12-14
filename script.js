abdb04de1695894fc8222ce73e1313c96ec1b3cf
Tim Torndal <tim.torndal@skola.botkyrka.se>
2020-12-14 14:05:35 +0100

    Jul

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvas = document.createElement('canvas')
+const MAX_WIDTH = document.body.clientWidth
+const MAX_Height = document.body.clientHeight
+
+canvas.width - document.body.clientWidth
+canvas.height = document.body.clientHeight
+document.appendChild(canvas)
+
+const ctx = canvas.getContext('2d')
+
+context.fillstyle = 'red'
+conttext.fillRect(0, 0, MAX_WIDTH, MAX_Height)  
+
